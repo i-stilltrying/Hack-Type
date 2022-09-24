dcbf3fd2d35db2c99400474db6ca2025a4056c4f
Aman Verma <sunnyaman331@gmail.com>
2022-09-24 23:31:03 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef, useState } from 'react';
+
+const c=5
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  const [sourceCode, setsourceCode] = useState("");
+  const [content, setContent] = useState("")
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const containerRef = useRef(null)
+
+  useEffect(() => {
+    containerRef.current.focus();
+
+
+    fetch('code.txt')
+      .then((res) => res.text())
+      .then((text) => setsourceCode(text));
+  }, [])
+
+  const runScript = () => {
+    setCurrentIndex(currentIndex+c)
+   setContent( sourceCode.substring(0,currentIndex))
+
+
+  }
+  const removeMessage = () => { window.location.reload(false)
+
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.key != 'Escape') runScript()
+    else removeMessage()
+  };
+
+
+
+
+  return (<>
+    <div className="container" onKeyDown={handleKeyDown} tabIndex={0} ref={containerRef}>
+
+      <div id="source">
+{content}
+      </div>
+
+    </div></>
   );
 }
 
